bb8aec0bf310f6378a01d747c1bb5e1d6248ec86
viktor-vad <viktor.vad@gmail.com>
2026-07-29 21:22:33 +0200

    SDL window

---
+++ 00_base_code/WindowMessageHook.cpp
@@ -0,0 +1,27 @@
+// Use the C++ standard templated min/max
+#define NOMINMAX
+
+// DirectX apps don't need GDI
+#define NODRAWTEXT
+#define NOGDI
+#define NOBITMAP
+
+// Include <mcx.h> if you need this
+#define NOMCX
+
+// Include <winsvc.h> if you need this
+#define NOSERVICE
+
+// WinHelp is deprecated
+#define NOHELP
+
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
+
+#include <Windows.h>
+
+bool __cdecl WindowsMessageHook( void* userdata, MSG* msg )
+{
+    return true;
+}
